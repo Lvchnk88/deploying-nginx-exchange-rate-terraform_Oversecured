702bde25bcc2c19128cdfde1366a5a86c2ab8354
Sergii Levchenko <levchenko88@gmail.com>
2023-06-19 20:29:01 +0300

    configure main.tf

--- main.tf
+++ main.tf
@@ -4,48 +4,50 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_vpc" "it_craft" {
+resource "aws_vpc" "oversecured_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "test_task"
+    Name = "tf-oversecured-vpc"
   }
 }
 
-resource "aws_subnet" "test_subnet" {
-  vpc_id                  = aws_vpc.it_craft.id
+resource "aws_subnet" "oversecured_subnet" {
+  vpc_id                  = aws_vpc.oversecured_vpc.id
   cidr_block              = "172.16.10.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
 
   tags = {
-    Name = "my_subnet"
+    Name = "tf-oversecured-subnet"
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.it_craft.id
+resource "aws_internet_gateway" "oversecured_gw" {
+  vpc_id = aws_vpc.oversecured_vpc.id
 
   tags = {
-    Name = "main"
+    Name = "tf-oversecured-gw"
   }
 }
 
-resource "aws_route_table" "public-crt" {
-  vpc_id = aws_vpc.it_craft.id
+resource "aws_route_table" "oversecured_public_rtb" {
+  vpc_id = aws_vpc.oversecured_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.oversecured_gw.id
   }
   tags = {
-    Name = "public-crt"
+    Name = "tf-oversecured-public-rtb"
   }
 }
 
-resource "aws_route_table_association" "crta-public_subnet-1" {
-  subnet_id      = aws_subnet.test_subnet.id
-  route_table_id = aws_route_table.public-crt.id
+resource "aws_route_table_association" "oversecured_crta_public_subnet" {
+  subnet_id      = aws_subnet.oversecured_subnet.id
+  route_table_id = aws_route_table.oversecured_public_rtb.id
 }
+
+====== continue ========
 
 resource "aws_security_group" "ssh-allowed" {
   vpc_id = aws_vpc.it_craft.id
